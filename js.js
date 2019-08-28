bab00cdfcf07b2ad3ad10095f17a7c039097226f
Crystal Conroy <csconroy@udel.edu>
2019-08-28 12:28:51 -0400

    New fields, formatting
    Added VIP and education section. Also updated experience for Gore
    internship.

--- js.js
+++ js.js
@@ -1,29 +1,29 @@
 // change navbar color when scrolling
-    $(function () {
-        $(document).scroll(function () {
-            var $nav = $(".navbar");
-            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        });
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
-    // smooth scroll above anchor
-    $(document).ready(function() {
-        $('a[href^="#"]').click(function() {
-            var target = $(this.hash);
-            if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
-            if (target.length == 0) target = $('html');
-            $('html, body').animate({ scrollTop: target.offset().top-100 }, 1000);
-            return false;
-        });
+});
+// smooth scroll above anchor
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var target = $(this.hash);
+        if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+        if (target.length == 0) target = $('html');
+        $('html, body').animate({ scrollTop: target.offset().top - 100 }, 1000);
+        return false;
     });
-    // change active tab while scrolling
-    $(window).scroll(function() {  
-        var scrollDistance = $(window).scrollTop();
-        
-            // Assign active class to nav links while scolling
-            $('.page-section').each(function(i) {
-                    if ($(this).position().top <= scrollDistance+200) {
-                            $('.navigation a').parent().removeClass('active');
-                            $('.navigation a').eq(i).parent().addClass('active');
-                    }
-            });
+});
+// change active tab while scrolling
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function (i) {
+        if ($(this).position().top <= scrollDistance + 200) {
+            $('.navigation a').parent().removeClass('active');
+            $('.navigation a').eq(i).parent().addClass('active');
+        }
     });
+});
